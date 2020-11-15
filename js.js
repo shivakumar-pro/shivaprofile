e19766c1d0a3450746c2996eeefd5062d39aa929
ShivaKumar <imshivakumar6@gmail.com>
2020-11-15 13:21:35 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,26 @@
+var circle = document.getElementById("circle");
+var upbtn = document.getElementById("upbtn");
+var downbtn = document.getElementById("downbtn");
+
+var rotateValue = circle.style.transform;
+var rotateSum;
+
+upbtn.onclick = function(){
+    rotateSum = rotateValue + "rotate(-90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+
+}
+downbtn.onclick = function(){
+    rotateSum = rotateValue + "rotate(90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+    
+}
+function myFunction() {
+    document.getElementById("myDIV").innerHTML = "<h1>7829531238</h1>";
+    var x = document.getElementById("myDIV");
+    
+    x.style.display = "myDIV";
+    
+  }
